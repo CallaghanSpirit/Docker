122c41011d52f3675690c7b2c618edbb57571fc3
Alan Nouman <tsoloevalan@gmail.com>
2025-04-14 17:07:09 +0300

    3

--- DRIVEPARTS-DOCKER/driveparts/gunicorn.py
+++ DRIVEPARTS-DOCKER/driveparts/gunicorn.py
@@ -1,10 +1,10 @@
 from os import environ
 import multiprocessing
 
-bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+bind = "0.0.0.0:" + environ.get("PORT", "8000")
+workers = multiprocessing.cpu_count()
 max_requests = 1000
-worker_class = "uvicorn.workers.UvicornWorker"
+worker_class = "gevent"
 reload = True
 timeout = 120
 
